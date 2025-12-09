768bb7c6ffdd096a53a40f45f3ee35e23920adfa
Zachary Liang <liangzachary20@gmail.com>
2025-12-08 17:27:34 -0800

    Hero section of Main Page done

--- src/components/home/HeroSection.jsx
+++ src/components/home/HeroSection.jsx
@@ -17,16 +17,21 @@ function HeroSection() {
       </video>
 
       {/* GLASS LAYER*/}
-      <div className="absolute inset-0 bg-white/40 backdrop-blur-2xl"></div>
+      <div className="absolute inset-0 bg-white/60 "></div>
 
       {/* FOREGROUND CONTENT */}
       <div className="relative z-10 flex flex-col items-center text-center gap-4 md:gap-6 px-4">
+      {/* Light Spot Behind Text
+      <div className="absolute z-10 top-1/3 w-[60%] h-[40%] 
+          bg-[radial-gradient(circle,rgba(255,255,255,0.6)_0%,rgba(255,255,255,0)_70%)]
+          blur-2xl pointer-events-none">
+      </div> */}
 
-        <h1 className="hero-title" style={{ color: '#1F4E79' }}>
+        <h1 className="hero-title max-w-[638px] leading-tight" style={{ color: '#1F4E79' }}>
           Secure the Future for Enterprise AI.
         </h1>
 
-        <h3 className="subheader mb-6 md:mb-12 text-lg md:text-2xl" style={{ color: '#4B4B4B' }}>
+        <h3 className="subheader max-w-[623px] mb-6 md:mb-12 text-lg md:text-2xl" style={{ color: '#4B4B4B' }}>
           Prompt-based protection that empowers safe, efficient AI adoption.
         </h3>
 
