1992b4ac9d9ccddde5491e53f9f573872b9dc903
TechnicalMonk <hello@peteshimshock.com>
2026-06-11 13:10:36 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-﻿import { Routes, Route } from 'react-router-dom';
+﻿import { Analytics } from '@vercel/analytics/react';
+import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import AnnouncementBar from './components/AnnouncementBar';
 import ScrollToTop from './components/ScrollToTop';
